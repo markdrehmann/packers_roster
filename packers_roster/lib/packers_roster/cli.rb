cf97399faf3a2cf55beedb7d03ced9d9a9cb554c
Mark Drehmann <markdrehmann@gmail.com>
2020-04-02 16:00:42 +0000

    player class edits

--- packers_roster/lib/packers_roster/cli.rb
+++ packers_roster/lib/packers_roster/cli.rb
@@ -1,6 +1,11 @@
 class PackersRoster::CLI
   def call
     puts "Welcome to the Packers Roster app!"
+    puts "To search for your favorite players' 2019 stats, enter:"
+    puts "  1. To search by name"
+    puts "  2. To search by number"
+    puts "  3. To search by position"
+    
 
     binding.pry
 
--- packers_roster/lib/packers_roster/scraper.rb
+++ packers_roster/lib/packers_roster/scraper.rb
@@ -1,4 +1,20 @@
 class PackersRoster::Scraper
   
+  def self.scrape_roster(roster_url)
+    doc = Nokogiri::HTML(open(roster_url))
+    array = []
+    doc.css("span.nfl-o-roster__player-name").each do |player|
+      # each player needs name and profile within a hash to add to array - need to assign within iteration to have them in the same hash
+      
+      card.css(".student-card a").each do |student| # . is for class
+        p_name = 
+        # = student.css(".student-location").text
+        p_profile = student.attr("href") # attr searches for "href" within 'student-card a'
+        array << {:name => p_name, :profile_url => p_profile}
+      end
+    end
+    array
+  end
+  
   
 end
