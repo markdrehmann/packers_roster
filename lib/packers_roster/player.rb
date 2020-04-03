063f4932df63640495ee99d8ea746f46031f963c
Mark Drehmann <markdrehmann@gmail.com>
2020-04-03 16:35:20 +0000

    player class initialize, create_from_collection'

--- lib/packers_roster/player.rb
+++ lib/packers_roster/player.rb
@@ -2,5 +2,25 @@ class PackersRoster::Player
   attr_accessor :name, :number, :position, :profile_url
   @@all = []
   
+  def initialize(player_hash)
+    player_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
+  def self.create_from_collection(# players_array - return value from scrape_roster, Scraper.scrape_roster?)
+    players_array.each do |player_hash|
+      PackersRoster::Player.new(player_hash)
+    end
+  end
+
+  # def add_student_attributes(attributes_hash)
+  #   attributes_hash.each do |key, value|
+  #     self.send(("#{key}="), value)
+  #   end
+  # end
+
+  def self.all
+    @@all
+  end
   
 end
