e44e6f639b5b7e9b24c3c7be50c5832ed9354ae0
Mark Drehmann <markdrehmann@gmail.com>
2020-04-03 13:58:16 +0000

    update scraper class

--- lib/packers_roster/player.rb
+++ lib/packers_roster/player.rb
@@ -1,5 +1,5 @@
 class PackersRoster::Player
-  attr_accessor :name, :number, :position
+  attr_accessor :name, :number, :position, :profile_url
   @@all = []
   
   
--- lib/packers_roster/scraper.rb
+++ lib/packers_roster/scraper.rb
@@ -4,17 +4,13 @@ class PackersRoster::Scraper
     doc = Nokogiri::HTML(open(roster_url))
     array = []
     doc.css("span.nfl-o-roster__player-name").each do |player|
-      # each player needs name and profile within a hash to add to array - need to assign within iteration to have them in the same hash
-      
-      card.css(".student-card a").each do |student| # . is for class
-        p_name = 
-        # = student.css(".student-location").text
-        p_profile = student.attr("href") # attr searches for "href" within 'student-card a'
-        array << {:name => p_name, :profile_url => p_profile}
-      end
+      p_name = player.text
+      p_profile = player.attr("href")
+      array << {:name => p_name, :profile_url => p_profile}
     end
     array
   end
-  
-  
+      
+      # each player needs name and profile within a hash to add to array - need to assign within iteration to have them in the same hash
+      
 end
