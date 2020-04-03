364886ac3d9dc5fa28f1a69b7427c891c2f90197
Mark Drehmann <markdrehmann@gmail.com>
2020-04-03 22:11:21 +0000

    cli edit

--- lib/packers_roster/cli.rb
+++ lib/packers_roster/cli.rb
@@ -1,7 +1,7 @@
 class PackersRoster::CLI
   def call
     puts "Welcome to the Packers Roster app!"
-    puts "To search for your favorite players' 2019 stats, enter:"
+    puts "For your favorite players' 2019 stats, enter:"
     puts "  1. To search by name"
     puts "  2. To search by number"
     puts "  3. To search by position"
