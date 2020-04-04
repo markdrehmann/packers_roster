e4daa1cf0bbcde5ad40383ae0e66df23b5009cb3
Mark Drehmann <markdrehmann@gmail.com>
2020-04-04 15:37:06 +0000

    built Scraper.scrape_profile method

--- lib/packers_roster/scraper.rb
+++ lib/packers_roster/scraper.rb
@@ -17,15 +17,22 @@ class PackersRoster::Scraper
   def self.scrape_profile(profile_url)
     p_site = "https://www.packers.com" + profile_url
     doc = Nokogiri::HTML(open(p_site))
-    stats_hash = {}
+    
     position = doc.css("h3.d3-o-media-object__primary-subtitle").text.strip
     number = doc.css("h3.d3-o-media-object__secondary-subtitle").text.strip.delete("#")
-    
+  
+    stats = doc.css("span.nfl-t-stats-tile__label-full").map do |stat|
+      stat.text.to_sym
+    end
+
+    stat_value = doc.css("div.nfl-t-stats-tile__value").map do |value|
+      value.text.strip
+    end
+
+    stats_hash = Hash[stats.zip(stat_value)]
     stats_hash[:position] = position
     stats_hash[:number] = number
-    
     stats_hash
-    
   end
 end
 
