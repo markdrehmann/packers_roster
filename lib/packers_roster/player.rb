eab42a1c6c4b37c98610ca022ed535178144af6e
Mark Drehmann <markdrehmann@gmail.com>
2020-04-07 13:17:04 +0000

    updated player#list_stats

--- lib/packers_roster/player.rb
+++ lib/packers_roster/player.rb
@@ -16,14 +16,14 @@ class PackersRoster::Player
   end
   
   def list_stats
-    puts @name
-    puts @number
-    puts @position
-    puts @height
-    puts @weight
-    puts @age
-    puts @experience
-    puts @college
+    puts name
+    puts "  Number      #{number}"
+    puts "  Position    #{position}"
+    puts "  Height      #{height}"
+    puts "  Weight      #{weight}"
+    puts "  Age         #{age}"
+    puts "  Experience  #{experience}"
+    puts "  College     #{college}"
   end
   
   def self.find_by_name(name)
