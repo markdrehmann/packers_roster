2ad764599cfed4d74db6c6098cab46752f7cb5a7
Mark Drehmann <markdrehmann@gmail.com>
2020-04-07 13:24:23 +0000

    moved scraping to beginning of CLI #call method

--- lib/packers_roster/cli.rb
+++ lib/packers_roster/cli.rb
@@ -1,14 +1,17 @@
 class PackersRoster::CLI
   def call
+    
+     ### this is my scraping to create players and their stats
+    PackersRoster::Scraper.scrape_roster
+    PackersRoster::Player.create_from_collection
+    
     puts "Welcome to the Packers Roster app!"
     puts "For your favorite players' 2019 stats, enter:"
     puts "  1. To search by name"
     puts "  2. To search by number"
     puts "  3. To search by position"
     
-    ### this is my scraping to create players and their stats
-    PackersRoster::Scraper.scrape_roster
-    PackersRoster::Player.create_from_collection
+   
     
     ### this is printing something out to make sure code is working
     puts PackersRoster::Player.all[40].list_stats
