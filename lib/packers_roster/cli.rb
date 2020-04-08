d0830d0332e310d399506e371e5b083e2e369681
Mark Drehmann <markdrehmann@gmail.com>
2020-04-08 20:15:49 +0000

    developing the cli

--- lib/packers_roster/cli.rb
+++ lib/packers_roster/cli.rb
@@ -6,29 +6,31 @@ class PackersRoster::CLI
     PackersRoster::Player.create_from_collection
     
     puts "Welcome to the Packers Roster app!"
+    
+    input = ""
+    until input == "4"
     puts "To learn more about your favorite players, enter:"
     puts "  1 - To search by name"
     puts "  2 - To search by number"
     puts "  3 - To search by position"
     puts "  4 - To quit"
     
-    ### this doesn't work yet
-    # input = gets.strip
-    
-    # if input == "1"
-    #   puts "Enter the player's name"
-    #   p_input = gets
-    #   PackersRoster::Player.find_by_name(p_input).list_stats
-    # elsif input == "4"
-    #   quit
-    # end
+      input = gets.strip
+      if input == "1"
+        puts "Enter the player's name"
+        p_input = gets.strip
+        PackersRoster::Player.find_by_name(p_input).list_stats
+      elsif input == "4"
+        puts "Thanks!"
+      end
+    end
       
     
     ### testing find_by_name method and list_stats
     # PackersRoster::Player.find_by_name("Aaron Rodgers").list_stats
     
     ### testing find_by_position method
-    PackersRoster::Player.find_by_position("WR").each {|p| puts p.name}
+    # PackersRoster::Player.find_by_position("WR").each {|p| puts p.name}
 
   end
   
