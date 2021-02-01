cbe2d7a8dfc79f8cbb38d905b364dbfbd0b24070
markdrehmann <45129297+markdrehmann@users.noreply.github.com>
2021-02-01 15:54:01 -0600

    Update scraper.rb

--- lib/packers_roster/scraper.rb
+++ lib/packers_roster/scraper.rb
@@ -3,12 +3,14 @@ class PackersRoster::Scraper
   def self.scrape_roster
     doc = Nokogiri::HTML(open("https://packers.com/team/players-roster"))
     array = []
+    
     doc.css("tr").each do |p_page|
         info = p_page.text.strip.split(" ")
         p_name = info[0..1].join(" ")
         p_college = info[8..-1].join(" ")
         array << {name: p_name, number: info[2], position: info[3], height: info[4], weight: info[5], age: info[6], experience: info[7], college: p_college}
     end
+    
     array.shift
     array
   end
